b48d929fd242824c0cd20d41a86deda25aa2c6c2
demurg <demurg1960@gmai.com>
2022-10-25 15:38:13 -0500

    proyecto actualizado

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -25,7 +25,6 @@ import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.
     HeroesRoutingModule,
     MaterialModule,
     FlexLayoutModule,
-    CiberLoginModule,
   ],
 })
 export class HeroesModule {}
